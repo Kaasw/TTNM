35b5577e7765c54c7c5b231170a2575ff722655d
Kaasw <hieu1282003@gmail.com>
2024-05-10 11:36:46 +0700

    Init commit

---
+++ backend/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import declarative_base
+
+# from .base import Base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(16), nullable=False, unique=True)
+    email = Column(String(255), nullable=False, unique=True)
+    password = Column(String(32), nullable=False)
+    full_name = Column(String(255), nullable=False)
+
---
+++ backend/schemas/user.py
@@ -0,0 +1,27 @@
+# Pydantic model
+
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    password: str
+    username: str
+    full_name: str
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserById(UserBase):
+    id: int
+    
+    class Config:
+        from_attributes = True
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class UserUpdate:
+    pass
+
+
