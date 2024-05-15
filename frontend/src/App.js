c956f525b20f0873a373dac21420c041263a64e7
Kaasw <hieu1282003@gmail.com>
2024-05-15 23:37:03 +0700

    Add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,16 @@
-import logo from './logo.svg';
-import React, { lazy, useEffect } from 'react'
-import './App.css';
-
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-  useEffect(() => {
-		document.title = "ADHD";
-	}, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
