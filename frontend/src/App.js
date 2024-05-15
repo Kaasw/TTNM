71768ae14ed921ab335a8f2ce5fb5e1d4ef3858b
Kaasw <hieu1282003@gmail.com>
2024-05-16 00:01:16 +0700

    First login screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screen/LoginScreen";
+
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" exact />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Navbar /> {/* Navbar outside the Routes */}
+
+      <Routes>  {/* Wrap all your routes inside the Routes component */}
+        <Route path="/" exact />
+        <Route path="/login" exact element={<LoginScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
