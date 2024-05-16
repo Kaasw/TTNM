8fedca92ab1e2a08597eb71d1749dd90d16d5cd0
Kaasw <hieu1282003@gmail.com>
2024-05-16 10:06:55 +0700

    Validation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginScreen from "./screen/LoginScreen";
+// import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import LoginScreen from "./screen/LoginScreen";
 
 function App() {
   return (
     <Router>
-      <Navbar /> {/* Navbar outside the Routes */}
 
-      <Routes>  {/* Wrap all your routes inside the Routes component */}
+
+      <Switch>  {/* Wrap all your routes inside the Routes component */}
         <Route path="/" exact />
-        <Route path="/login" exact element={<LoginScreen />} />
-      </Routes>
+        {/* <Route path="/login" exact element={<LoginScreen />} /> */}
+      </Switch>
     </Router>
   );
 }
